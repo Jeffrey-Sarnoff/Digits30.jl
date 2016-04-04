32c6983579c20d7503503ba5d81b0670d9f0ace2
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-04 08:46:47 -0400

    Update Digits30.jl

--- src/type/Digits30.jl
+++ src/type/Digits30.jl
@@ -12,14 +12,14 @@ Digit30(AsIs,hi::Float64) = Digit30(AsIs,hi,0.0)
 D30{T<:Float64}(hi::T, lo::T) = Digit30(AsIs,hi,lo)
 D30{T<:Float64}(hi::T) = Digit30(AsIs,hi)
 
-function Digit30{T<:Float64}(hi::T, lo::T) # hi,lo = eftAdd(hi,lo)
+function Digit30{T<:Float64}(a::T, b::T) # hi,lo = eftAdd(a,b)
   hi = a + b
   t = hi - a
   lo = (a - (hi - t)) + (b - t)
   Digit30(AsIs,hi,lo)
 end
 
-function Dig30{T<:Float64}(a::T, b::T) # hi,lo = eftAddGTE(hi,lo)
+function Dig30{T<:Float64}(a::T, b::T) # hi,lo = eftAddGTE(a,b)
   hi = a + b
   lo = b - (hi - a)
   Digit30(AsIs,hi,lo)
