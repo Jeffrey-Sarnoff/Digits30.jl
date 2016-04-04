91ce408cd73ecb1a3cce778a4d0b4603ba0a420a
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-04 08:41:53 -0400

    Update Digits30.jl

--- src/type/Digits30.jl
+++ src/type/Digits30.jl
@@ -25,7 +25,7 @@ function Dig30{T<:Float64}(a::T, b::T) # hi,lo = eftAddGTE(hi,lo)
   Digit30(AsIs,hi,lo)
 end
 
-show(io::IO, x::Digit30)
+show{T<:Digit30}(io::IO, x::T)
    s = string("Digit30(",x.hi,", ",x.lo,")")
    print(io, s)
 end
