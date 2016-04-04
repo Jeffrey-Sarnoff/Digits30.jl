f51b1f08ab7bd4db53e802486f0cb212a8a50251
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-04 12:12:00 -0400

    Update ops.jl

--- src/arith/ops.jl
+++ src/arith/ops.jl
@@ -1,19 +1,19 @@
 # addition
 
 function (+){T<:Digit30}(a::T,b::T)
-    s1, s2 = eftSum2(a.hi,b.hi)
-    t1, t2 = eftSum2(a.lo,b.lo)
+    s1, s2 = eftAdd(a.hi,b.hi)
+    t1, t2 = eftAdd(a.lo,b.lo)
     s2 += t1
-    s1, s2 = eftSum2inOrder(s1,s2)
+    s1, s2 = eftAddGTE(s1,s2)
     s2 += t2
-    s1, s2 = eftSum2inOrder(s1,s2)
+    s1, s2 = eftAddGTE(s1,s2)
     Digit30(s1,s2)
 end
 
 function (+)(a::Digit30,b::Float64)
-    s1, s2 = eftSum2(a.hi,b)
+    s1, s2 = eftAdd(a.hi,b)
     s2 += a.lo
-    s1, s2 = eftSum2inOrder(s1,s2)
+    s1, s2 = eftAddGTE(s1,s2)
     Digit30(s1,s2)
 end
 
@@ -24,26 +24,26 @@ end
 # subtraction
 
 function (-){T<:Digit30}(a::T,b::T)
-    s1, s2 = eftDiff2(a.hi,b.hi)
-    t1, t2 = eftDiff2(a.lo,b.lo)
+    s1, s2 = eftSub(a.hi,b.hi)
+    t1, t2 = eftSub(a.lo,b.lo)
     s2 += t1
-    s1, s2 = eftSum2inOrder(s1,s2)
+    s1, s2 = eftAddGTE(s1,s2)
     s2 += t2
-    s1, s2 = eftSum2inOrder(s1,s2)
+    s1, s2 = eftAddGTE(s1,s2)
     Digit30(s1,s2)
 end
 
 function (-)(a::Digit30,b::Float64)
-    s1, s2 = eftDiff2(a.hi,b)
+    s1, s2 = eftSub(a.hi,b)
     s2 += a.lo
-    s1, s2 = eftSum2inOrder(s1,s2)
+    s1, s2 = eftAddGTE(s1,s2)
     Digit30(s1,s2)
 end
 
 function (-)(a::Float64,b::Digit30)
-    s1, s2 = eftDiff2(a,b.hi)
+    s1, s2 = eftSub(a,b.hi)
     s2 -= b.lo
-    s1, s2 = eftSum2inOrder(s1,s2)
+    s1, s2 = eftAddGTE(s1,s2)
     Digit30(s1,s2)
 end
 
@@ -54,29 +54,29 @@ end
 
 
 function (sqr)(a::Digit30)
-  t1,t2 = eftProd2(a.hi,a.hi)
+  t1,t2 = eftMul(a.hi,a.hi)
   t3 = a.hi * a.lo
   t5 = t3 + t3
   t6 = t2 + t5
-  t1,t6 = eftSum2inOrder(t1,t6)
+  t1,t6 = eftAddGTE(t1,t6)
   Digit30(t1,t6)
 end
 
 function (*){T<:Digit30}(a::T,b::T)
-  t1,t2 = eftProd2(a.hi,b.hi)
+  t1,t2 = eftMul(a.hi,b.hi)
   t3 = a.hi * b.lo
   t4 = a.lo * b.hi
   t5 = t3 + t4
   t6 = t2 + t5
-  t1,t6 = eftSum2inOrder(t1,t6)
+  t1,t6 = eftAddGTE(t1,t6)
   Digit30(t1,t6)
 end
 
 function (*)(a::Digit30,b::Float64)
-  t1,t2 = eftProd2(a.hi,b)
+  t1,t2 = eftMul(a.hi,b)
   t4 = a.lo * b
   t6 = t2 + t4
-  t1,t6 = eftSum2inOrder(t1,t6)
+  t1,t6 = eftAddGTE(t1,t6)
   Digit30(t1,t6)
 end
 
@@ -86,21 +86,17 @@ end
 
 
 function fma{T<:Digit30}(a::T,b::T,c::T)
-    p = a*TD(b)
-    p = p+c
-    Digit30(p.hi,p.md)
+    hi,lo = eftFMAto2(a,b,c)
+    Digit30(hi,lo)
 end
 
 
 # reciprocation
 
-#=
-# faster, less accurate
-=#
 
 function (recip)(b::Digit30)
   q1 = one(Float64) / b.hi
-  r  = Digit30(one(TD) - q1*TD(b))
+  r  = one(Digit30) - q1*b
 
   q2 = r.hi / b.hi
   r = r - (q2 * b)
@@ -109,8 +105,8 @@ function (recip)(b::Digit30)
 
   q3 = r.hi / b.hi
 
-  q1,q2 = eftSum2inOrder(q1, q2)
-  q1,q2 = eftSum3as2(q1,q2,q3)
+  q1,q2 = eftAdd(q1, q2)
+  q1,q2 = eftAdd3to2(q1,q2,q3)
   Digit30(q1,q2)
 end
 
@@ -125,8 +121,8 @@ function (/){T<:Digit30}(a::T,b::T)
 
   q3 = r.hi / b.hi
 
-  q1,q2 = eftSum2inOrder(q1, q2)
-  q1,q2 = eftSum3as2(q1,q2,q3)
+  q1,q2 = eftAddGTE(q1, q2)
+  q1,q2 = eftAdd3to2(q1,q2,q3)
   Digit30(q1,q2)
 end
 
