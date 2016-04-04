2d5639dfbf3c3f1c49907b907c96e523b3b4b137
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-04 09:00:54 -0400

    Update Digits30.jl

--- src/Digits30.jl
+++ src/Digits30.jl
@@ -4,6 +4,8 @@ import Base: convert, promote_rule, hash, string, show
 
 export Digit30, Dig30, D30
 
+using ErrorfreeArithmetic
+
 include("type/concrete.jl")
 include("type/convert.jl")
 include("type/io.jl")
