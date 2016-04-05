338e0c44216e88855ceb28e7c1e828c04c1eb3c6
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-04 20:19:57 -0400

    Update concrete.jl

--- src/type/concrete.jl
+++ src/type/concrete.jl
@@ -1,20 +1,20 @@
-immutable Digit30 <: AbstractFloat
+immutable Float120 <: AbstractFloat
     hi::Float64
     lo::Float64
     
-    Digit30(hi::Float64, lo::Float64) = new(hi,lo)
+    Float120(hi::Float64, lo::Float64) = new(hi,lo)
 end
 
-hi(x::Digit30) = x.hi
-lo(x::Digit30) = x.lo
+hi(x::Float120) = x.hi
+lo(x::Float120) = x.lo
 
-Digit30(hi::Float64) = Digit30(hi,0.0)
+Float120(hi::Float64) = Float120(hi,0.0)
 
 
 # fully clean a,b then set hi,lo
-function D30{T<:Float64}(a::T, b::T) # hi,lo = eftAdd(a,b)
+function F120{T<:Float64}(a::T, b::T) # hi,lo = eftAdd(a,b)
   hi = a + b
   t = hi - a
   lo = (a - (hi - t)) + (b - t)
-  Digit30(hi,lo)
+  Float120(hi,lo)
 end
