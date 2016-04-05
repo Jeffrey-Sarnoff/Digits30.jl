5d7aff81944eef275a58821d5ef26db6b9e7572f
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-04 20:35:51 -0400

    Update comparatives.jl

--- src/type/comparatives.jl
+++ src/type/comparatives.jl
@@ -1,32 +1,33 @@
-
-function isless(a::Digit30, b::Digit30)
+function isless(a::Float120, b::Float120)
     (a.hi < b.hi) || (a.hi==b.hi && a.lo<b.lo)
 end
-function isless(a::Digit30, b::Float64)
+function isless(a::Float120, b::Float64)
     (a.hi < b) || (a.hi==b && a.lo<zero(Float64))
 end
-function isless(a::Float64, b::Digit30)
+function isless(a::Float64, b::Float120)
     (a < b.hi) || (a==b.hi && b.lo<zero(Float64))
 end
-function isequal(a::Digit30, b::Digit30)
+function isequal(a::Float120, b::Float120)
     (a.hi == b.hi) && (a.lo == b.lo)
 end
-function isequal(a::Digit30, b::Float64)
+function isequal(a::Float120, b::Float64)
     (a.hi == b) && (a.lo == zero(Float64))
 end
-function isequal(a::Float64, b::Digit30)
+function isequal(a::Float64, b::Float120)
     (a == b.hi) && (b.lo == zero(Float64))
 end
 
-@inline (==)(a::Digit30,b::Digit30) = (a.hi == b.hi) && (a.lo == b.lo)
-@inline (< )(a::Digit30,b::Digit30) = (a.hi < b.hi) || (a.hi==b.hi && a.lo<b.lo)
-@inline (<=)(a::Digit30,b::Digit30) = (a.hi < b.hi) || (a.hi==b.hi && a.lo<=b.lo)
-@inline (> )(a::Digit30,b::Digit30) = (a.hi > b.hi) || (a.hi==b.hi && a.lo>b.lo)
-@inline (>=)(a::Digit30,b::Digit30) = (a.hi > b.hi) || (a.hi==b.hi && a.lo>=b.lo)
+@inline (==)(a::Float120,b::Float120) = (a.hi == b.hi) && (a.lo == b.lo)
+@inline (< )(a::Float120,b::Float120) = (a.hi < b.hi) || (a.hi==b.hi && a.lo<b.lo)
+@inline (<=)(a::Float120,b::Float120) = (a.hi < b.hi) || (a.hi==b.hi && a.lo<=b.lo)
+@inline (> )(a::Float120,b::Float120) = (a.hi > b.hi) || (a.hi==b.hi && a.lo>b.lo)
+@inline (>=)(a::Float120,b::Float120) = (a.hi > b.hi) || (a.hi==b.hi && a.lo>=b.lo)
 
-min{T<:Digit30}(a::T, b::T) = (a<b) ? a : b
-max{T<:Digit30}(a::T, b::T) = (b<a) ? a : b
-minmax{T<:Digit30}(a::T, b::T) = (a<b) ? (a,b) : (b,a)
-maxmin{T<:Digit30}(a::T, b::T) = (b<a) ? (a,b) : (b,a)
+min{T<:Float120}(a::T, b::T) = (a<b) ? a : b
+max{T<:Float120}(a::T, b::T) = (b<a) ? a : b
+minmax{T<:Float120}(a::T, b::T) = (a<b) ? (a,b) : (b,a)
+maxmin{T<:Float120}(a::T, b::T) = (b<a) ? (a,b) : (b,a)
+
+
 
 
