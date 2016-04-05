724c30c2c1e5ada52bd8756ff0b772b14eeb6772
Jeffrey Sarnoff <Jeffrey-Sarnoff@users.noreply.github.com>
2016-04-04 20:39:18 -0400

    Update primitive.jl

--- src/type/primitive.jl
+++ src/type/primitive.jl
@@ -1,43 +1,43 @@
-sign(a::Digit30) = sign(a.hi)
-@inline signbit(a::Digit30) = signbit(a.hi)
+sign(a::Float120) = sign(a.hi)
+@inline signbit(a::Float120) = signbit(a.hi)
 
 #=
     negation, abs, flipsign, copysign
 =#
 
-@inline (-)(a::Digit30) = Digit30(-a.hi, -a.lo)
-@inline abs(a::Digit30) = ifelse(signbit(a.hi), -a, a)
+@inline (-)(a::Float120) = Float120(-a.hi, -a.lo)
+@inline abs(a::Float120) = ifelse(signbit(a.hi), -a, a)
 
-flipsign(a::Digit30, b::Digit30) = ifelse(signbit(b.hi), -a, a)
-flipsign{T<:Real}(a::Digit30, b::T) = ifelse(signbit(b), -a, a)
-copysign(a::Digit30, b::Digit30) = ifelse(signbit(b.hi), -abs(a), abs(a))
-copysign{T<:Real}(a::Digit30, b::T) = ifelse(signbit(b), -abs(a), abs(a))
+flipsign(a::Float120, b::Float120)   = ifelse(signbit(b.hi), -a, a)
+flipsign{T<:Real}(a::Float120, b::T) = ifelse(signbit(b), -a, a)
+copysign(a::Float120, b::Float120)   = ifelse(signbit(b.hi), -abs(a), abs(a))
+copysign{T<:Real}(a::Float120, b::T) = ifelse(signbit(b), -abs(a), abs(a))
 
 
 #=
     frexp, ldexp, zero, one
 =#
 
-function frexp(a::Digit30)
+function frexp(a::Float120)
     frhi, xphi = frexp(a.hi)
     frlo, xplo = frexp(a.lo)
-    Digit30(frhi, ldexp(frlo,xplo-xphi)), xphi
+    Float120(frhi, ldexp(frlo,xplo-xphi)), xphi
 end
 
-function ldexp(a::Digit30,xp::Int)
-    Digit30(ldexp(a.hi,xp),ldexp(a.lo,xp))
+function ldexp(a::Float120,xp::Int)
+    Float120(ldexp(a.hi,xp),ldexp(a.lo,xp))
 end
-ldexp{I<:Integer}(fx::Tuple{Digit30,I}) = ldexp(fx...)
+ldexp{I<:Integer}(fx::Tuple{Float120,I}) = ldexp(fx...)
 
 
-zero(::Type{Digit30}) = Digit30(0.0, 0.0)
-one(::Type{Digit30}) = Digit30(1.0, 0.0)
-zero(x::Digit30) = zero(Digit30)
-one(x::Digit30) = one(Digit30)
+zero(::Type{Float120}) = Float120(0.0, 0.0)
+one(::Type{Float120})  = Float120(1.0, 0.0)
+zero(x::Float120) = zero(Float120)
+one(x::Float120)  = one(Float120)
 
 # Inf, NaN
 
-Inf30(::Type{Digit30}) = Digit30(Inf(T), Inf(T))
-NaN30(::Type{Digit30}) = Digit30(NaN(T), NaN(T))
-Inf30(x::Digit30) = Inf30(Digit30)
-NaN30(x::Digit30) = NaN30(Digit30)
+Inf30(::Type{Float120}) = Float120(Inf(T), Inf(T))
+NaN30(::Type{Float120}) = Float120(NaN(T), NaN(T))
+Inf30(x::Float120) = Inf30(Float120)
+NaN30(x::Float120) = NaN30(Float120)
